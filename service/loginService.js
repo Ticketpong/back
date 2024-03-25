163f07170a8f94320229ebe345c8b27a44462320
YeoniSon <sjy8211@gmail.com>
2024-03-25 10:26:35 +0900

    feat: login/ logout
    login과 logout 작성 loginController, loginService 파일로 분할해 작성
    login할때 생기는 cookie를 cookies.json파일에 저장함

---
+++ service/loginService.js
@@ -0,0 +1,57 @@
+const dbconn = require("../config/mariadb");
+let bcrypt = require("bcrypt-nodejs");
+
+const memberLogin = (id, pw, next) => {
+  let sql = `SELECT * FROM MEMBER WHERE user_id = ?`;
+  console.log(sql);
+
+  return new Promise((resolve, reject) => {
+    dbconn.db.query(sql, [id], (err, result) => {
+      if (err || result.length === 0) {
+        console.log("아이디 불일치");
+        resolve(false);
+      }
+
+      console.log(result[0].user_id, result[0].user_password);
+      bcrypt.compare(pw, result[0].user_password, (err, isMatch) => {
+        console.log(isMatch);
+        if (err || !isMatch) {
+          console.log("비밀번호 불일치");
+          resolve(false);
+        } else {
+          console.log("비밀번호 일치");
+          resolve(true);
+        }
+      });
+    });
+  });
+};
+
+const manageLogin = (id, pw, next) => {
+  let sql = `SELECT * FROM MEMBER WHERE manage_id = ?`;
+
+  console.log(sql);
+
+  return new Promise((resolve, reject) => {
+    dbconn.db.query(sql, [id], (err, result) => {
+      if (err || result.length === 0) {
+        console.log("아이디 불일치");
+        resolve(false);
+      }
+
+      console.log(result[0].user_id, result[0].user_password);
+
+      bcrypt.compare(pw, result[0].user_password, (err, isMatch) => {
+        if (err || !isMatch) {
+          console.log("비밀번호 불일치");
+          resolve(false);
+        } else {
+          console.log("비밀번호 일치");
+          resolve(true);
+        }
+      });
+    });
+  });
+};
+
+module.exports = { memberLogin, manageLogin };
