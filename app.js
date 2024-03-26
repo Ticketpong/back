364e973f04010d16c5529123504a45c9ca8772d9
Uddoro <lowell0322@naver.com>
2024-03-26 22:34:43 +0900

    feat: db insert

--- app.js
+++ app.js
@@ -10,69 +10,6 @@ const bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
 
-// views 폴더 설정 및 ejs 엔진 설정
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "ejs");
-
-// 미들웨어 설정
-app.use(logger("dev")); // 로그 출력
-app.use(express.json()); // json 형식으로 데이터를 받아오기 위한 설정
-app.use(express.urlencoded({ extended: false })); // form 형식으로 데이터를 받아오기 위한 설정
-app.use(cookieParser()); // 쿠키 파싱 설정
-app.use(express.static(path.join(__dirname, "public"))); // 정적 파일 위치 설정
-app.use(
-  session({
-    secret: "keyboard",
-    resave: false,
-    saveUninitialized: true,
-    store: new FileStore(),
-  })
-);
-
-// 라우터 설정
-const indexRouter = require("./routes/index");
-const signupRouter = require("./routes/member/signup");
-const loginRouter = require("./routes/member/login");
-const mainRouter = require("./routes/member/main");
-const logoutRouter = require("./routes/member/logout");
-const manageLogin = require("./routes/manage/manageLogin");
-const manageAdd = require("./routes/manage/manageAdd");
-const manageMain = require("./routes/manage/manageMain");
-const manageLogout = require("./routes/manage/manageLogout");
-
-//라우터 연결
-app.use("/", indexRouter);
-app.use("/signup", signupRouter);
-app.use("/login", loginRouter);
-app.use("/main", mainRouter);
-app.use("/logout", logoutRouter);
-app.use("/manage", manageLogin);
-app.use("/manage/manageAdd", manageAdd);
-app.use("/manage/manageMain", manageMain);
-app.use("/manage/manageLogout", manageLogout);
-
-//에러 핸들러
-// 404 에러 핸들러
-app.use((req, res, next) => {
-  const error = new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
-  error.status = 404;
-  next(error);
-});
-
-// 서버쪽 에러 핸들러
-app.use((err, req, res, next) => {
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
-  res.status(err.status || 500);
-  res.render("error");
-});
-
-// 서버 실행
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
-});
-
-
 
 
 /////////////// 공연 API ////////////////
@@ -82,7 +19,7 @@ app.use(logger('dev')); // 로그 출력
 app.use(express.json()); // json 형식으로 데이터를 받아오기 위한 설정
 app.use(express.urlencoded({ extended: false })); // form 형식으로 데이터를 받아오기 위한 설정
 app.use(cookieParser()); // 쿠키 파싱 설정
-app.use(express.static(path.join(__dirname, 'public'))); // 정적 파일 위치 설정
+app.use(express.static(path.join(__dirname, 'public'))); // 정적 파일 위치 u
 app.use(session({
     secret: 'keyboard',
     resave: false,
@@ -92,11 +29,11 @@ app.use(session({
 app.use(bodyParser.json()); // JSON 파싱을 위한 미들웨어
 
 // View 엔진 및 뷰 경로 설정
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+//app.set('views', path.join(__dirname, 'views'));
+//app.set('view engine', 'ejs');
 
 // 라우터 설정
-const apiRouter = require('../routes/api.js');
+const apiRouter = require('./route/api.js');
 app.use('/api', apiRouter);
 
 // 에러 핸들러
---
+++ route/api.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+const { saveShowData, saveBoxoffice } = require('../controller/apiController');
+
+// '/getPerformancesData' 경로로 GET 요청을 받을 때 두 함수를 병렬로 실행합니다.
+router.get('/getPerformancesData', (req, res) => {
+  Promise.all([saveShowData()]) // , saveBoxoffice() 나중에 []아안에 추가
+    .then(() => {
+      res.status(200).send('Data saved successfully.');
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(500).send('Error occurred while saving data.');
+    });
+});
+
+module.exports = router;
