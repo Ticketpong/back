d1226cc2ada24b60e2f272841fc1312d1fa34313
YeoniSon <sjy8211@gmail.com>
2024-03-27 13:19:50 +0900

    fix: 중복 내용 삭제및 정리

--- app.js
+++ app.js
@@ -27,6 +27,7 @@ app.use(
     store: new FileStore(),
   })
 );
+app.use(bodyParser.json()); // JSON 파싱을 위한 미들웨어
 
 // 라우터 설정
 const indexRouter = require("./routes/index");
@@ -42,6 +43,7 @@ const manageLogin = require("./routes/manage/manageLogin");
 const manageAdd = require("./routes/manage/manageAdd");
 const manageMain = require("./routes/manage/manageMain");
 const manageLogout = require("./routes/manage/manageLogout");
+const apiRouter = require("./routes/api.js");
 
 //라우터 연결
 app.use("/", indexRouter);
@@ -51,6 +53,7 @@ app.use("/signup", signupRouter);
 app.use("/login", loginRouter);
 app.use("/main", mainRouter);
 app.use("/logout", logoutRouter);
+app.use("/api", apiRouter);
 
 //manage 라우터 연결
 app.use("/manage", manageLogin);
@@ -78,51 +81,6 @@ app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
 
-
-
-/////////////// 공연 API ////////////////
-
-const port = 8080; // 서버포트
-
-// 미들웨어 설정
-app.use(logger('dev')); // 로그 출력
-app.use(express.json()); // json 형식으로 데이터를 받아오기 위한 설정
-app.use(express.urlencoded({ extended: false })); // form 형식으로 데이터를 받아오기 위한 설정
-app.use(cookieParser()); // 쿠키 파싱 설정
-app.use(express.static(path.join(__dirname, 'public'))); // 정적 파일 위치 u
-app.use(session({
-    secret: 'keyboard',
-    resave: false,
-    saveUninitialized: true,
-    store: new FileStore(),
-}));
-app.use(bodyParser.json()); // JSON 파싱을 위한 미들웨어
-
-// View 엔진 및 뷰 경로 설정
-//app.set('views', path.join(__dirname, 'views'));
-//app.set('view engine', 'ejs');
+/////////////// 공연 API ///////////////////////
 
 // 라우터 설정
-const apiRouter = require('./routes/api.js');
-app.use('/api', apiRouter);
-
-// 에러 핸들러
-// 404 에러 핸들러
-app.use((req, res, next) => {
-    const error = new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
-    error.status = 404;
-    next(error);
-});
-
-// 에러 핸들러
-app.use((err, req, res, next) => {
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
-    res.status(err.status || 500);
-    res.render('error');
-});
-
-// 서버 실행 포트 코드
-app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
-});
