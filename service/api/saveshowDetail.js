349ebaa2b097022a618135be9e2d4f96468833df
Uddoro <lowell0322@naver.com>
2024-03-30 00:19:32 +0900

    chore: 3 layers

--- service/api/saveshowDetail.js
+++ service/api/saveshowDetail.js
@@ -3,7 +3,6 @@ const xml2json = require("xml-js");
 
 const { db } = require("../../config/mariadb"); // DB 정보 require
 
-
 // API에서 공연 정보를 가져와서 데이터베이스에 저장하는 함수
 function saveShowData() {
   console.log("공연정보 먼저실행");
@@ -28,12 +27,14 @@ function saveShowData() {
     const info = JSON.parse(
       xml2json.xml2json(body, { compact: true, spaces: 4 })
     );
-   
-    const showIds = info.dbs.db.map((item) => item.mt20id ? item.mt20id._text : null);
+
+    const showIds = info.dbs.db.map((item) =>
+      item.mt20id ? item.mt20id._text : null
+    );
 
     console.log(showIds);
 
-    if(showIds != null) {
+    if (showIds != null) {
       showIds.forEach((showId) => {
         getShowDetail(showId);
       });
@@ -61,15 +62,13 @@ function getShowDetail(showId) {
       return;
     }
 
-
     // api 호출 결과
     const detailInfo = JSON.parse(
       xml2json.xml2json(body, { compact: true, spaces: 4 })
     );
 
-   
     const item = detailInfo.dbs.db; // dbs안에 db라는 이름의 리스트에서 데이터 꺼냄
-   
+
     let styurl = "";
     if (item.styurls && item.styurls.styurl) {
       if (Array.isArray(item.styurls.styurl)) {
@@ -79,46 +78,45 @@ function getShowDetail(showId) {
       }
     }
 
-
     // 외래키로 연결되는 데이터들은 id값을 가져와서 저장
     //const manage_id = 'manager';
     //const managerIdQuery = `SELECT id FROM MANAGE WHERE manage_id = ?`;
-    
+
     //db.query(managerIdQuery, manage_id, function (err, manageResult) {
     //  if (err) {
     //    console.error("Error inserting data:", err);
     //    return;
     //  }
 
-          //const manegerId = manageResult[0].id;
-          const sql = `INSERT INTO PERFORMANCE (prfnm, manager_id, mt10id, mt20id,  prfpdfrom, prfpdto, prfruntime, pcseguidance, genrenm, prfstate, updatedate, poster, styurl, dtguidance, prfage) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`;
-          const values = [
-            item.prfnm._text, // 공연제목
-            'manager', //  아이디
-            item.mt10id._text, // 공연시설ID "mt10id"
-            item.mt20id._text, // 공연아이디 "mt20id"
-            item.prfpdfrom._text, // 공연시작일
-            item.prfpdto._text, // 공연종료일
-            item.prfruntime._text, // 공연 런타임
-            item.pcseguidance._text, // 티켓가격
-            item.genrenm._text, // 장르
-            item.prfstate._text, // 공연상태
-            item.updatedate ? item.updatedate._text : null, // 최종수정일
-            item.poster._text, // 포스터 이미지 경로
-            styurl, // 소개 이미지
-            item.dtguidance._text, // 공연시간
-            item.prfage._text, // 관람등급
-          ];
-          // 쿼리 실행
-          db.query(sql, values, function (err, result) {
-            if (err) {
-              console.error("Error inserting data:", err);
+    //const manegerId = manageResult[0].id;
+    const sql = `INSERT INTO PERFORMANCE (prfnm, manager_id, mt10id, mt20id,  prfpdfrom, prfpdto, prfruntime, pcseguidance, genrenm, prfstate, updatedate, poster, styurl, dtguidance, prfage) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`;
+    const values = [
+      item.prfnm._text, // 공연제목
+      "manager", //  아이디
+      item.mt10id._text, // 공연시설ID "mt10id"
+      item.mt20id._text, // 공연아이디 "mt20id"
+      item.prfpdfrom._text, // 공연시작일
+      item.prfpdto._text, // 공연종료일
+      item.prfruntime._text, // 공연 런타임
+      item.pcseguidance._text, // 티켓가격
+      item.genrenm._text, // 장르
+      item.prfstate._text, // 공연상태
+      item.updatedate ? item.updatedate._text : null, // 최종수정일
+      item.poster._text, // 포스터 이미지 경로
+      styurl, // 소개 이미지
+      item.dtguidance._text, // 공연시간
+      item.prfage._text, // 관람등급
+    ];
+    // 쿼리 실행
+    db.query(sql, values, function (err, result) {
+      if (err) {
+        console.error("Error inserting data:", err);
 
-              return;
-            }
-            console.log("Data inserted successfully.");
-          });
+        return;
+      }
+      console.log("Data inserted successfully.");
     });
+  });
   //});
 }
 
