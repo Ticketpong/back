0efc876d4b7573b28b32f4b68a3ca570bbeedd4f
YeoniSon <sjy8211@gmail.com>
2024-03-29 17:57:14 +0900

    fix: loginpage 연결

--- routes/member/login.js
+++ routes/member/login.js
@@ -4,8 +4,6 @@ const login = require("../../controllers/loginController");
 
 router.get("/", login.getLogin);
 
-router.get("/member/login", login.getLogin);
-
 router.post("/", login.memberLogin);
 
 module.exports = router;
