301452435498b29547f384198c2ea761e7cc5ecf
YeoniSon <sjy8211@gmail.com>
2024-04-03 23:34:05 +0900

    fix: api 불러오기, db저장 구현

--- service/api/hallService.js
+++ service/api/hallService.js
@@ -6,7 +6,7 @@ const savePlaceId = async () => {
   try {
     console.log("savePlaceId");
     const options = await axios.get(
-      "http://www.kopis.or.kr/openApi/restful/prfplc?service=3fe13cc59eba4d328faa4d0c7dff5b3e&cpage=1&rows=1000"
+      "http://www.kopis.or.kr/openApi/restful/prfplc?service=3fe13cc59eba4d328faa4d0c7dff5b3e&cpage=1&rows=3000"
     );
 
     const xmlData = options.data;
@@ -17,12 +17,15 @@ const savePlaceId = async () => {
       setTimeout(() => {
         console.log("10초 후 실행");
       }, 10000);
-      return item.mt10id ? item.mt10id._text : null;
+      return {
+        mt10id: item.mt10id ? item.mt10id._text : null,
+        sidonm: item.sidonm ? item.sidonm._text : null,
+      };
     });
 
     if (placeList != null) {
-      placeList.forEach((placeId) => {
-        getPlaceDetail(placeId);
+      placeList.forEach(({ mt10id, sidonm }) => {
+        getPlaceDetail(mt10id, sidonm);
       });
       console.log("mt10id 가져오기 완료");
     }
@@ -31,11 +34,11 @@ const savePlaceId = async () => {
   }
 };
 
-const getPlaceDetail = async (placeId) => {
+const getPlaceDetail = async (mt10id, sidonm) => {
   try {
     // console.log("getPlaceDetail");
     const response = await axios.get(
-      `http://www.kopis.or.kr/openApi/restful/prfplc/${placeId}`,
+      `http://www.kopis.or.kr/openApi/restful/prfplc/${mt10id}`,
       {
         params: {
           service: "3fe13cc59eba4d328faa4d0c7dff5b3e",
@@ -50,13 +53,15 @@ const getPlaceDetail = async (placeId) => {
 
     const placeDetail = result.dbs.db;
 
-    const sql = `INSERT INTO PERFORMANCEHALL (mt10id, fcltynm, telno, la, lo) VALUES (?, ?, ?, ?, ?)`;
+    const sql = `INSERT INTO PERFORMANCEHALL (mt10id, fcltynm, sidonm, telno, la, lo, adres) VALUES (?, ?, ?, ?, ?, ?, ?)`;
     const params = [
-      placeId, // mt10id
-      placeDetail.fcltynm._text, // fcltynm
+      mt10id, // mt10id
+      placeDetail.fcltynm._text || "-", // fcltynm
+      sidonm, // sidonm
       placeDetail.telno._text ? placeDetail.telno._text : "-", // telno
       placeDetail.la._text, // la
       placeDetail.lo._text, // lo
+      placeDetail.adres._text || "-", // adres
     ];
     console.log(params);
     // DB에 저장
