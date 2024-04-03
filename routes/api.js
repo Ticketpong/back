e553d00a0566282087be353996ef0c934a54e896
YeoniSon <sjy8211@gmail.com>
2024-04-03 09:33:17 +0900

    feat: api PERFORMANCEHALL 가져오기 완료

--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const { saveBoxOffice } = require("../service/api/saveBoxOffice");
-const { savePlaceInfo } = require("../service/api/savePlaceInfo");
-const { getSaveHallList } = require("../service/api/savePlaceInfo");
 const { saveShowData } = require("../service/api/saveshowDetail");
+const { getHallDetail } = require("../controllers/apiController");
 
 // 공연 상세정보 저장 라우트
 router.get("/saveShowData", async (req, res) => {
@@ -19,33 +18,8 @@ router.get("/saveShowData", async (req, res) => {
   }
 });
 
-// 시설 정보 저장 라우트
-router.get("/getSaveHallList", async (req, res) => {
-  try {
-    await getSaveHallList(); // savePlaceInfo 함수 호출 및 완료 대기
-
-    // 성공적으로 작업이 완료되면 200 상태 코드와 함께 응답 보냄
-    res.status(200).send("Place info saved successfully");
-  } catch (error) {
-    // 에러가 발생하면 500 상태 코드와 함께 에러 메시지 응답
-    console.error("Error saving place info:", error);
-    res.status(500).send("Internal server error");
-  }
-});
-
-// 시설 정보 저장 라우트
-router.get("/savePlaceInfo", async (req, res) => {
-  try {
-    await savePlaceInfo(); // savePlaceInfo 함수 호출 및 완료 대기
-
-    // 성공적으로 작업이 완료되면 200 상태 코드와 함께 응답 보냄
-    res.status(200).send("Place info saved successfully");
-  } catch (error) {
-    // 에러가 발생하면 500 상태 코드와 함께 에러 메시지 응답
-    console.error("Error saving place info:", error);
-    res.status(500).send("Internal server error");
-  }
-});
+// get 공연시설 상세정보
+router.get("/hallDetail", getHallDetail);
 
 /// 예매상황판 정보 저장 라우트
 router.get("/saveBoxOffice", async (req, res) => {
