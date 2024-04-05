437758c05c2dd471a64c18d63f684d248c57d74b
YeoniSon <sjy8211@gmail.com>
2024-04-05 17:04:06 +0900

    fix: 로그인 session 수정

--- routes/member/login.js
+++ routes/member/login.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const login = require("../../controllers/loginController");
 
-router.get("/", login.getLogin);
+router.get("/profile", login.getLogin);
 
 router.post("/", login.memberLogin);
 
