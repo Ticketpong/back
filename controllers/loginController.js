360686cb44ccaa2533c930347ce10716f445e514
YeoniSon <sjy8211@gmail.com>
2024-04-07 20:16:04 +0900

    fix: login session을 token으로 변경

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -1,4 +1,11 @@
 const loginService = require("../service/loginService");
+const jwt = require("jsonwebtoken");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const secretKey = process.env.SECRET_KEY;
+const options = JSON.parse(process.env.OPTIONS);
 
 // POST login
 // 로그인 처리
@@ -8,10 +15,9 @@ const memberLogin = async (req, res, next) => {
     let result = await loginService.memberLogin(id, pw);
     console.log(result);
     if (result) {
-      req.session.userId = id;
-      req.session.isLogined = true;
-      req.session.save();
-      res.status(200).send("login success");
+      const isLogined = true;
+      const token = jwt.sign({ id, isLogined }, secretKey, options);
+      res.status(200).json({ token, isLogined, message: "login success" });
     } else {
       res
         .status(400)
@@ -25,38 +31,26 @@ const memberLogin = async (req, res, next) => {
 
 // GET login
 // 로그인 페이지 렌더링
-const getLogin = (req, res, next) => {
-  const { userId, isLogined } = req.session;
-  if (isLogined) {
-    res.status(200).json({ userId, isLogined });
+const getLogin = async (req, res, next) => {
+  const token = req.headers.authorization?.split(" ")[1]; // 헤더에서 토큰 추출
+  try {
+    const decoded = jwt.verify(token, secretKey); // 토큰 검증
+    res.status(200).json({ id: decoded.id, isLogined: decoded.isLogined });
+  } catch (error) {
+    res.status(401).json({ message: "로그인이 필요합니다." });
   }
-  res.status(400).send("login failed");
 };
 
 // Get logout
 // 로그아웃 처리
 const getLogout = (req, res, next) => {
   try {
-    req.session.destroy((err) => {
-      if (err) {
-        console.log(err);
-        return res.status(500).send("logout failed");
-      }
-      return res.status(200).json({ isLogined: false });
-    });
+    res.clearCookie("token");
+    res.status(200).json({ isLogined: false });
   } catch (error) {
     console.log(error);
     res.status(500).send("logout failed");
   }
-};
-
-//get manageLogin
-const getManageLogin = (req, res, next) => {
-  const { manageId, isLogined } = req.session;
-  if (isLogined) {
-    res.status(200).json({ manageId, isLogined });
-  }
-  res.status(400).send("login failed");
 };
 
 //post manageLogin
@@ -65,10 +59,9 @@ const postManageLogin = async (req, res, next) => {
   try {
     let result = await loginService.manageLogin(id, pw);
     if (result) {
-      req.session.manageId = id;
-      req.session.isLogined = true;
-      req.session.save();
-      res.status(200).send("login success");
+      const isLogined = true;
+      const token = jwt.sign({ id, isLogined }, secretKey, options);
+      res.status(200).json({ token, isLogined, message: "login success" });
     } else {
       res
         .status(400)
@@ -80,16 +73,22 @@ const postManageLogin = async (req, res, next) => {
   }
 };
 
+//get manageLogin
+const getManageLogin = (req, res, next) => {
+  const token = req.headers.authorization?.split(" ")[1]; // 헤더에서 토큰 추출
+  try {
+    const decoded = jwt.verify(token, secretKey); // 토큰 검증
+    res.status(200).json({ id: decoded.id, isLogined: decoded.isLogined });
+  } catch (error) {
+    res.status(401).json({ message: "로그인이 필요합니다." });
+  }
+};
+
 //get mangeLogout
 const getManageLogout = (req, res, next) => {
   try {
-    req.session.destroy((err) => {
-      if (err) {
-        console.log(err);
-        return res.status(500).send("logout failed");
-      }
-      return res.status(200).json({ isLogined });
-    });
+    res.clearCookie("token");
+    res.status(200).json({ isLogined: false });
   } catch (error) {
     console.log(error);
     res.status(500).send("logout failed");
