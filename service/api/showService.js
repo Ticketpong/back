4452e4e02b9d85a99f82e95d8b13446aec437376
YeoniSon <sjy8211@gmail.com>
2024-04-12 03:24:10 +0900

    refactor: showService api 가독성 높이는 코드 수정

--- service/api/showService.js
+++ service/api/showService.js
@@ -57,20 +57,6 @@ const getShowDetail = async (showId) => {
 
     const showDetail = result.dbs.db;
 
-    // const isValidGenre = (genre) => {
-    //   const validGenre = ["연극"];
-    //   return validGenre.includes(genre);
-    // };
-
-    // // 장르가 연극, 뮤지컬, 대중음악인 경우에만 저장
-    // const genre = showDetail.genrenm._text;
-    // if (!isValidGenre(genre)) {
-    //   console.log("유효하지 않은 장르:", genre);
-    //   return; // 장르가 유효하지 않으면 저장하지 않음
-    // } else {
-    //   return genre;
-    // }
-
     let styurl = "";
 
     if (showDetail.styurls && showDetail.styurls.styurl) {
@@ -82,88 +68,111 @@ const getShowDetail = async (showId) => {
     }
 
     const post = true;
-    let cnt = 0;
 
     const manageIdQuery = `SELECT * FROM MANAGE WHERE manage_id= ?`;
     const manageId = "manage1";
 
     new Promise((resolve, reject) => {
-      dbconn.db.query(manageIdQuery, manageId, (err, manageResult) => {
+      dbconn.db.query(manageIdQuery, manageId, async (err, manageResult) => {
         if (err) {
           console.error("MANAGE 테이블 조회 실패:", err);
           reject(err);
         }
 
-        const Query = `SELECT * FROM PERFORMANCE WHERE mt20id = ?`;
+        const query = `SELECT * FROM PERFORMANCE WHERE mt20id = ?`;
         const params = [showId];
 
-        dbconn.db.query(Query, params, (err, result) => {
-          if (err) {
-            console.error("DB 조회 실패:", err);
+        const result = await getPerformance(query, params);
+
+        if (result.length === 0) {
+          const placeQuery = `SELECT * FROM PERFORMANCEHALL WHERE mt10id = ?`;
+          const placeParams = [showDetail.mt10id._text];
+          const placeResult = await getPlace(placeQuery, placeParams);
+          if (err || placeResult.length === 0) {
             reject(err);
-          }
-          console.log("DB 조회 완료");
-          if (result.length === 0) {
-            const query = `SELECT * FROM PERFORMANCEHALL WHERE mt10id = ?`;
-            const param = [showDetail.mt10id._text];
-            dbconn.db.query(query, param, (err, result) => {
-              if (err) {
-                console.error("DB 조회 실패:", err);
-                reject(err);
-              } else if (result.length === 0) {
-                console.log("없는 데이터");
-              } else {
-                const sql = `INSERT INTO PERFORMANCE (mt20id, manage_id, mt10id, prfnm, prfpdfrom, prfpdto, prfruntime, pcseguidance, genrenm, prfstate, updatedate, poster, styurl, dtguidance, post, prfage) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)`;
+          } else {
+            const saveSql = `INSERT INTO PERFORMANCE (mt20id, manage_id, mt10id, prfnm, prfpdfrom, prfpdto, prfruntime, pcseguidance, genrenm, prfstate, updatedate, poster, styurl, dtguidance, post, prfage) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)`;
 
-                const params = [
-                  showId, // mt20id
-                  manageId, // manage_id
-                  showDetail.mt10id._text ? showDetail.mt10id._text : "-", // mt10id
-                  showDetail.prfnm._text ? showDetail.prfnm._text : "-", // prfnm
-                  showDetail.prfpdfrom._text ? showDetail.prfpdfrom._text : "-", // prfpdfrom
-                  showDetail.prfpdto._text ? showDetail.prfpdto._text : "-", // prfpdto
-                  showDetail.prfruntime._text
-                    ? showDetail.prfruntime._text
-                    : "-", // prfruntime
-                  showDetail.pcseguidance._text
-                    ? showDetail.pcseguidance._text
-                    : "-", // pcseguidance
-                  showDetail.genrenm._text ? showDetail.genrenm._text : "-", // genrenm
-                  // genre, // genrenm
-                  showDetail.prfstate._text ? showDetail.prfstate._text : "-", // prfstate
-                  showDetail.updatedate._text
-                    ? showDetail.updatedate._text
-                    : "-", // updatedate
-                  showDetail.poster._text ? showDetail.poster._text : "-", // poster
-                  styurl, // styurl
-                  showDetail.dtguidance._text
-                    ? showDetail.dtguidance._text
-                    : "-", // dtguidance
-                  post, // post
-                  showDetail.prfage._text, // prfage
-                ];
-                dbconn.db.query(sql, params, (err, result) => {
-                  if (err) {
-                    console.error("DB 저장 실패:", err);
-                    // return;
-                    reject(err);
-                  }
-                  console.log("DB 저장 완료");
-                  cnt++;
+            const saveParams = [
+              showId, // mt20id
+              manageId, // manage_id
+              showDetail.mt10id._text, // mt10id
+              showDetail.prfnm._text, // prfnm
+              showDetail.prfpdfrom._text, // prfpdfrom
+              showDetail.prfpdto._text, // prfpdto
+              showDetail.prfruntime._text, // prfruntime
+              showDetail.pcseguidance._text, // pcseguidance
+              showDetail.genrenm._text, // genrenm
+              showDetail.prfstate._text, // prfstate
+              showDetail.updatedate._text, // updatedate
+              showDetail.poster._text, // poster
+              styurl, // styurl
+              showDetail.dtguidance._text, // dtguidance
+              post, // post
+              showDetail.prfage._text, // prfage
+            ];
 
-                  resolve(result);
-                });
-              }
-            });
+            const showDetailResult = await saveShowDetail(saveSql, saveParams);
+            if (showDetailResult) {
+              resolve(result);
+            } else {
+              reject(err);
+            }
           }
-        });
+        }
       });
     });
-    console.log("공연장상세정보 완료");
-    console.log(cnt);
   } catch (error) {
-    console.error("공연장상세정보 에러:", error);
+    console.error("공연장 상세정보 저장 실패:", error);
+    return "error";
   }
+};
+
+// 공연 정보 받아오기
+const getPerformance = async (query, params) => {
+  return new Promise((resolve, reject) => {
+    dbconn.db.query(query, params, async (err, result) => {
+      if (err) {
+        console.error("DB 조회 실패:", err);
+        reject(err);
+      }
+      console.log("DB 조회 완료");
+      if (result.length === 0) {
+        resolve(result);
+      }
+    });
+  });
+};
+
+// 공연장 정보 받아오기
+const getPlace = async (query, params) => {
+  return new Promise((resolve, reject) => {
+    dbconn.db.query(query, params, async (err, result) => {
+      if (err) {
+        console.error("DB 조회 실패:", err);
+        reject(err);
+      } else if (result.length === 0) {
+        console.log("없는 데이터");
+        reject(err);
+      } else {
+        resolve(result);
+      }
+    });
+  });
+};
+
+// 공연장 정보 저장
+const saveShowDetail = async (sql, params) => {
+  return new Promise((resolve, reject) => {
+    dbconn.db.query(sql, params, (err, result) => {
+      if (err) {
+        console.error("DB 저장 실패:", err);
+        reject(err);
+      }
+      console.log("DB 저장 완료");
+      resolve(result);
+    });
+  });
 };
 
 module.exports = { saveShowId };
