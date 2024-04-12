820f130d628e1fc26c78758b9d4d2fb13b197f79
YeoniSon <sjy8211@gmail.com>
2024-04-12 11:25:56 +0900

    refactor: get => post

--- routes/searchBar.js
+++ routes/searchBar.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const searchBar = require("../controllers/searchBarController");
 
 // 검색창 (http://localhost:8080/searchBar)
-router.get("/", searchBar.search);
+router.post("/", searchBar.search);
 
 module.exports = router;
