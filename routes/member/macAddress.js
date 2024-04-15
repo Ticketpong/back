30affbd2db48bc3d914f549c6e05e0cfb8755a27
YeoniSon <sjy8211@gmail.com>
2024-04-15 17:46:36 +0900

    docs : macAddress 주석 수정

--- routes/member/macAddress.js
+++ routes/member/macAddress.js
@@ -2,13 +2,13 @@ const express = require("express");
 const router = express.Router();
 const macAddress = require("../../controllers/macAddressController");
 
-// macAddress 저장 (http://localhost:3000/macAddress)
+// macAddress 저장 (http://localhost:8080/macAddress)
 router.post("/", macAddress.create);
 
-// macAddress 가져오기 (http://localhost:3000/macAddress/profile)
+// macAddress 가져오기 (http://localhost:8080/macAddress/profile)
 router.post("/profile", macAddress.get);
 
-// macAddress 수정 (http://localhost:3000/macAddress/edit)
+// macAddress 수정 (http://localhost:8080/macAddress/edit)
 router.put("/edit", macAddress.update);
 
 module.exports = router;
