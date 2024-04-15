3a7733390c30690770705f48e41280c1c5f8ef48
YeoniSon <sjy8211@gmail.com>
2024-04-15 10:35:17 +0900

    refactor: searchbar 수정

--- routes/searchBar.js
+++ routes/searchBar.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const searchBar = require("../controllers/searchBarController");
 
 // 검색창 (http://localhost:8080/searchBar)
-router.post("/", searchBar.search);
+router.get("/", searchBar.search);
 
 module.exports = router;
